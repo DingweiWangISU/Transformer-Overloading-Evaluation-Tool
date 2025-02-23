0283f6d231590df6dc125b57a24ee24738825020
Aronnax, Miles V. <rassilon@iastate.edu>
2025-02-23 12:51:11 -0600

    Initial page flow.

--- web.py
+++ web.py
@@ -3,16 +3,26 @@
 # 
 #
 from eprc.tfoverload_tool import TFOverload_Tool
-from flask import Flask, render_template
+from flask import Flask, request, render_template
 
 app = Flask(__name__)
-#tfot = TFOverload_Tool()
 
 
 @app.route("/")
 def landing_page():
     return render_template("landing.html")
     #return "inspector"
+
+
+@app.route("/form", methods = ['GET', 'POST'])
+def form_page():
+    if request.method == 'GET':
+        return render_template("form.html")
+
+    if request.method == 'POST':
+        #tfot = TFOverload_Tool()
+        #return render_template("calc.html")
+        return "Not implemented"
 
 
 # Deal with robots
