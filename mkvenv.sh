042806863bf73810c284e271f6bea92779fe1419
Aronnax, Miles V. <rassilon@iastate.edu>
2025-02-24 11:20:26 -0600

    Make branding toggle, start defining user form data.

--- mkvenv.sh
+++ mkvenv.sh
@@ -4,8 +4,16 @@
 # a python virtual environment that can be used.
 #
 
-# Only create if there isn't an existing ${APP_VENV}
+# Load our environment.
 source common.sh
+
+# If "clean" is passed, delete existing ${APP_VENV}
+if [ "$1" == "clean" ]; then
+  echo "Cleaning (removing) old python virtual environment (${APP_VENV})."
+  rm -rf "${APP_VENV}"
+fi
+
+# Only create if there isn't an existing ${APP_VENV}
 if [ ! -d "${APP_VENV}" ]; then
   python3 -m venv ${APP_VENV}
   source ${APP_VENV}/bin/activate
--- web.py
+++ web.py
@@ -5,6 +5,7 @@
 # Expected Enivornment Variables:
 #  - WA_SECRET_KEY
 #  - WA_OUTPUT_DIR
+#  - WA_ISU_BRANDING
 #
 import os, uuid
 from werkzeug.utils import secure_filename
@@ -18,9 +19,8 @@ from cachelib.file import FileSystemCache
 app = Flask(__name__)
 app.config['SECRET_KEY'] = os.getenv('WA_SECRET_KEY', default='THISISNOTSAFE238493418')
 app.config['UPLOAD_FOLDER'] = os.getenv('WA_OUTPUT_DIR', default='output')
-ALLOWED_EXTENSIONS = {'xlsx'}
 
-# Configure session tracking
+# Configure server-side session tracking
 app.config['SESSION_PERMANENT'] = False
 app.config['SESSION_USE_SIGNER'] = True
 app.config['SESSION_TYPE'] = 'cachelib'
@@ -28,6 +28,10 @@ app.config['SESSION_SERIALIZATION_FORMAT'] = 'json'
 app.config['SESSION_FILE_DIR'] = f"{app.config['UPLOAD_FOLDER']}/sessions"
 app.config['SESSION_CACHELIB'] = FileSystemCache(threshold=512, cache_dir=app.config['SESSION_FILE_DIR'])
 Session(app)
+
+# Misc settings
+ALLOWED_EXTENSIONS = {'xlsx'}
+USE_ISU_BRANDING = True if os.getenv('WA_ISU_BRANDING', default='T') == "T" else False
 
 
 # Get session uuid (or create on if not set).
@@ -42,12 +46,20 @@ def is_allowed_file(filename):
     return '.' in filename and filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
 
 
+# Only require one captcha per session. 
+def has_existing_captcha():
+    if not 'captcha_done' in session:
+        return "N"
+    else:
+        return "Y"
+
+
 # The landing page should describe the purpose of the 
 # application and how to generate the input files.
 @app.route("/")
 def page_landing():
     user_session_uuid = get_session_uuid()
-    return render_template("landing.html")
+    return render_template("landing.html", USE_ISU_BRANDING=USE_ISU_BRANDING)
 
 
 # The form page is the heart of the application. It
@@ -58,18 +70,29 @@ def page_form():
     user_session_uuid = get_session_uuid()
     form_fallback     = False
 
+    # Set form defaults.
+    userformdata = {
+      "penetration_hp": 10,
+      "penetration_ev": 20,
+      "file_amidata": "",
+      "file_tfcinfo": "",
+      "captcha_done": has_existing_captcha()
+    }
+
     # Handle POST case first. We'll fall back to input form
     # if there's something wrong with the user supplied data.
     if request.method == 'POST':
+
         #tfot = TFOverload_Tool(args.amidata, args.tcinfo, args.evpen, args.hppen, args.output)
         # calc should show results, allow result download, and show a truncated form for 
         # redoing the calculation with different options.
-        #return render_template("calc.html")
+        #return render_template("calc.html", USE_ISU_BRANDING=USE_ISU_BRANDING, userformdata=userformdata)
         return "Not implemented"
 
     # Either show form or fallback to form input because of a problem.
     if request.method == 'GET' or form_fallback:
-        return render_template("form.html")
+        print(f"brandit={USE_ISU_BRANDING}")
+        return render_template("form.html", USE_ISU_BRANDING=USE_ISU_BRANDING, userformdata=userformdata)
 
 
 # Deal with robots
